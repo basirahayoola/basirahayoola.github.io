4f6a70bb61f9cb82b50ca403a09a9b0085443680
basirahayoola <bassy.tolly@gmail.com>
2022-10-16 13:03:12 +0100

    update

---
+++ js/gallery.js
@@ -0,0 +1,52 @@
+new Swiper(".swiper-container", {
+  speed: 400,
+  spaceBetween: 100,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+
+
+
+
+
+
+// let currentlySelected = 0;
+// const nodes = document.querySelectorAll(".gallery-img");
+// const prevBtn = document.querySelector(".prev");
+// const nextBtn = document.querySelector(".next");
+
+// function previous() {
+//   nextBtn.disabled = false;
+//   nodes[currentlySelected].classList.remove("active");
+//   currentlySelected--;
+//   nodes[currentlySelected].classList.add("active");
+
+//   if (currentlySelected === 0) {
+//     prevBtn.disabled = true;
+//   }
+// }
+
+// function next() {
+//   prevBtn.disabled = false;
+//   nodes[currentlySelected].classList.remove("active");
+//   currentlySelected++;
+//   nodes[currentlySelected].classList.add("active");
+
+//   if (currentlySelected + 1 === nodes.length) {
+//     nextBtn.disabled = true;
+//   }
+// }
+
+// function init() {
+//   prevBtn.addEventListener("click", function() {
+//     previous();
+//   });
+
+//   nextBtn.addEventListener("click", function(e) {
+//     next();
+//   });
+// }
+
+// init();
